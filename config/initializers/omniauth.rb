5163be80cda1142a1bfa93aa42202996504ef9e4
Imran Raja <imranraja85@gmail.com>
2014-02-09 02:04:36 -0500

    remove facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '132263183619935', 'd0ddda12df6e258b8f597cb28d07ee4a'
-end
+#OmniAuth.config.logger = Rails.logger
+#
+#Rails.application.config.middleware.use OmniAuth::Builder do
+#  provider :facebook, '132263183619935', 'd0ddda12df6e258b8f597cb28d07ee4a'
+#end
